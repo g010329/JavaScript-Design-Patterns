0129eca37d6bbfc84ae6bf09d9664267d2385d36
Grace <g010329@gmail.com>
2020-12-13 15:19:29 +0800

    1-6. Controlling the Global Access Completely

--- JSDP/section1/start/js/script.js
+++ JSDP/section1/start/js/script.js
@@ -1,60 +1,63 @@
-var chatModule = (function () {
-  var _leadself = "Me:  ",
-    _leadcomputer = "PC:   ",
-    _aSaid = ["This is a Cyber Chat"],
-    _msgYes = "Yes, that's a great idea.",
-    _msgNo = "No, that must be a mistake.",
-    _aSassyStuff = [
-      "Like mold on books, grow myths on history.",
-      "She moved like a poem and smiled like a sphinx.",
-      "As long as we don’t die, this is gonna be one hell of a story.",
-      "She laughed, and the desert sang.",
-      "You’ve got about as much charm as a dead slug.",
-    ];
+(function (win, doc, $) {
+  var chatModule = (function () {
+    var _leadself = "Me:  ",
+      _leadcomputer = "PC:   ",
+      _aSaid = ["This is a Cyber Chat"],
+      _msgYes = "Yes, that's a great idea.",
+      _msgNo = "No, that must be a mistake.",
+      _aSassyStuff = [
+        "Like mold on books, grow myths on history.",
+        "She moved like a poem and smiled like a sphinx.",
+        "As long as we don’t die, this is gonna be one hell of a story.",
+        "She laughed, and the desert sang.",
+        "You’ve got about as much charm as a dead slug.",
+      ];
 
-  function _echo(msg) {
-    _aSaid.push("<div>" + msg + "</div>");
+    function _echo(msg) {
+      _aSaid.push("<div>" + msg + "</div>");
 
-    var aSaidLength = _aSaid.length,
-      start = Math.max(_aSaid.length - 6, 0),
-      out = "";
+      var aSaidLength = _aSaid.length,
+        start = Math.max(_aSaid.length - 6, 0),
+        out = "";
 
-    for (var i = start; i < aSaidLength; i++) {
-      out += _aSaid[i];
+      for (var i = start; i < aSaidLength; i++) {
+        out += _aSaid[i];
+      }
+
+      $(".advert").html(out);
+      $("#talk span").text(msg);
+    }
+    function talk(msg) {
+      _echo(_leadself + msg);
+    }
+    function replyYesNo() {
+      var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
+      _echo(_leadcomputer + msg);
+    }
+    function saySassyStuff() {
+      var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
+      _echo(_leadcomputer + msg);
     }
 
-    $(".advert").html(out);
-    $("#talk span").text(msg);
-  }
-  function talk(msg) {
-    _echo(_leadself + msg);
-  }
-  function replyYesNo() {
-    var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
-    _echo(_leadcomputer + msg);
-  }
-  function saySassyStuff() {
-    var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
-    _echo(_leadcomputer + msg);
-  }
+    return {
+      talk: talk,
+      replyYesNo: replyYesNo,
+      saySassyStuff: saySassyStuff,
+    };
+  })();
 
-  return {
-    talk: talk,
-    replyYesNo: replyYesNo,
-    saySassyStuff: saySassyStuff,
-  };
-})();
+  $(doc).ready(function () {
+    chatModule.talk("this is graet");
+    chatModule.replyYesNo();
+    chatModule.saySassyStuff();
+  });
 
-$(document).ready(function () {
-  chatModule.talk("this is graet");
-  chatModule.replyYesNo();
-  chatModule.saySassyStuff();
-});
+  if (!win.chatModule) {
+    win.chatModule = chatModule;
+  }
+})(window, document, jQuery);
 
-// Reveal module :
-// Can't override those public methods anymore because we don't really have access to them.
-// We have access to a referance to them.
-// So it's a much more fragile design where it's not really extensible once it's created.
+console.log(window.chatModule);
 
 // ------------------
 // var com = com || {};
